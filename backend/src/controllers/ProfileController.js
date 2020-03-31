322cdcc13e79201bd9426310d86582742206dc40
Rafael Korz <rafael.korz@gmail.com>
2020-03-30 22:32:44 -0300

    Ajustado verificacao de Headers da ong

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -2,7 +2,7 @@ const connection = require('../database/connection');
 
 module.exports = {
   async index(resquest, response) {
-    const ong_id = request.headers.autorization;
+    const ong_id = resquest.headers.authorization;
 
     const incidents = await connection('incidents')
       .where('ong_id', ong_id)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,7 +3,7 @@ const { celebrate, Segments, Joi } = require('celebrate');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
@@ -12,21 +12,9 @@ routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 
-routes.post('/ongs', celebrate({
-  [Segments.BODY]: Joi.object().keys({
-    name: Joi.string().required(),
-    email: Joi.string().required().email(),
-    whatsapp: Joi.string().required().min(10).max(11),
-    city: Joi.string().required(),
-    uf: Joi.string().required().length(2),
-  })
-}), OngController.create);
+routes.post('/ongs', OngController.create);
 
-routes.get('/profile',celebrate({
-  [Segments.HEADERS]: Joi.object({
-    autorization: Joi.object().required(),
-  }).unknown(),
-}), ProfileController.index);
+routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', celebrate({
   [Segments.QUERY]: Joi.object({
